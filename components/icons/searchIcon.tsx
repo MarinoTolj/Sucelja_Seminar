50cf520561eed9fc4b8ccd22298a1c22f0caaa73
MarinoTolj <marino.tolj7@gmail.com>
2023-01-29 11:50:34 +0100

    feat: search bar

--- components/icons/searchIcon.tsx
+++ components/icons/searchIcon.tsx
@@ -1,11 +1,18 @@
 import Icon from "@mdi/react";
 import { mdiMagnify } from "@mdi/js";
+import { useRouter } from "next/router";
+import Link from "next/link";
 
-const SearchIcon = () => {
+const SearchIcon = (props: { searchValue: string }) => {
+  const router = useRouter();
+
+  const onClick = () => {
+    router.push;
+  };
   return (
-    <div className="bg-gray-300">
-      <Icon path={mdiMagnify} size={0.5} style={{ marginTop: "5px" }} />
-    </div>
+    <Link href={`/AllCategories/${props.searchValue}`} className="bg-gray-300">
+      <Icon path={mdiMagnify} size={0.5} style={{ marginTop: "5 px" }} />
+    </Link>
   );
 };
 export default SearchIcon;
--- components/searchBar.tsx
+++ components/searchBar.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 import { LegacyRef, useEffect, useRef, useState } from "react";
 import { searchCategories } from "../types/searchCategories";
 import SearchIcon from "./icons/searchIcon";
@@ -7,36 +8,48 @@ type PropType = {};
 
 const SearchBar: React.FC<PropType> = (props) => {
   const [searchValue, setSearchValue] = useState("");
-  const [userSearched, setUserSearched] = useState(false);
+  const [userClicked, setUserClicked] = useState(0);
   const inputRef = useRef<HTMLInputElement>(null);
+  const router = useRouter();
 
-  useEffect(() => {
-    console.log("hello");
-  }, [searchValue]);
+  const handleSearch = () => {
+    router.push(`/AllCategories/${searchValue}`);
+  };
+
+  const handleInputClick = () => {
+    setUserClicked((prev) => prev + 1);
+  };
 
   return (
     <div className="flex self-center flex-col">
       <div className="flex self-center">
-        <input
-          ref={inputRef}
-          placeholder="Search"
-          onChange={(e) => setSearchValue(e.currentTarget.value)}
-        />
-        <SearchIcon />
+        <form onSubmit={handleSearch}>
+          <input
+            ref={inputRef}
+            placeholder="Search"
+            onChange={(e) => setSearchValue(e.currentTarget.value)}
+            onClick={handleInputClick}
+          />
+        </form>
+        <SearchIcon searchValue={searchValue} />
       </div>
-
-      {searchValue && document.activeElement === inputRef.current && (
-        <div className="bg-white flex flex-col border-solid border-gray-300 border-2">
-          {searchCategories.map((category, index) => (
-            <button key={index}>
+      {userClicked >= 1 && document?.activeElement === inputRef.current && (
+        <div className="bg-white flex flex-col border-solid border-gray-300 border-2 z-10">
+          {searchCategories
+            .filter((category) =>
+              category
+                .toLocaleLowerCase()
+                .startsWith(searchValue.toLocaleLowerCase())
+            )
+            .map((category, index) => (
               <Link
+                key={index}
                 href={`/AllCategories/${category}`}
-                onClick={() => setUserSearched((prev) => !prev)}
+                className="text-center"
               >
-                {category}
+                <button>{category}</button>
               </Link>
-            </button>
-          ))}
+            ))}
         </div>
       )}
     </div>
