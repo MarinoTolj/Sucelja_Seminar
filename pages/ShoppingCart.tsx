26ad25744eed64e9917f54c54223352d074fa386
MarinoTolj <marino.tolj7@gmail.com>
2023-02-12 13:59:23 +0100

    more work on shopping cart

--- pages/ShoppingCart.tsx
+++ pages/ShoppingCart.tsx
@@ -12,7 +12,7 @@ const ShoppingCart: React.FC<PropType> = (props) => {
   return (
     <div>
       <h2 className="text-center mt-4 text-xl">
-        Your Total:{" "}
+        Your Total:
         {products.reduce(
           (acc, product) => product.price * product.amount + acc,
           0
@@ -23,7 +23,7 @@ const ShoppingCart: React.FC<PropType> = (props) => {
         {products.map((product, index) => {
           return (
             <>
-              <div className="mt-3 flex" key={index}>
+              <div className="mt-3 flex justify-between mx-2" key={index}>
                 <div className="w-16 h-16">
                   <Photo
                     src={`/photos/${product.name}.jpg`}
@@ -31,18 +31,33 @@ const ShoppingCart: React.FC<PropType> = (props) => {
                     clickable
                   />
                 </div>
-                <p>{product.name}</p>
-                <button onClick={() => dispatch(removeProduct(product.name))}>
-                  <Icon path={mdiMinus} size={1} />
-                </button>
-                <p>{product.amount}</p>
-                <button
-                  onClick={() =>
-                    dispatch(addProduct({ ...product, amount: 1 }))
-                  }
-                >
-                  <Icon path={mdiPlus} size={1} />
-                </button>
+                <div className="flex flex-col">
+                  <p className="text-2xl">{product.name}</p>
+                  <p className="font-semibold">{product.price}€</p>
+                  <div className="flex gap-2">
+                    Total: <p className="">{product.price * product.amount}€</p>
+                  </div>
+                </div>
+
+                <div className="flex gap-1">
+                  <button
+                    onClick={() => dispatch(removeProduct(product.name))}
+                    className="border-2 border-black h-12 self-center"
+                  >
+                    <Icon path={mdiMinus} size={1} />
+                  </button>
+                  <p className="self-center text-center text-2xl border-2 border-gray-500 px-3 h-12 pt-1">
+                    {product.amount}
+                  </p>
+                  <button
+                    onClick={() =>
+                      dispatch(addProduct({ ...product, amount: 1 }))
+                    }
+                    className="border-2 border-black h-12 self-center"
+                  >
+                    <Icon path={mdiPlus} size={1} />
+                  </button>
+                </div>
               </div>
               <hr className="bg-slate-600" />
             </>
