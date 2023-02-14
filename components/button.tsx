162a1eedb91fc2ae8074bcc28b1e6a640ed1642c
Jmilas00 <72361860+Jmilas00@users.noreply.github.com>
2023-02-14 11:20:20 +0100

    Feat: Login page done
    Co-authored-by: MarinoTolj <MarinoTolj@users.noreply.github.com>

--- components/button.tsx
+++ components/button.tsx
@@ -7,7 +7,7 @@ const Button: React.FC<ButtonHTMLAttributes<HTMLButtonElement>> = ({
 }) => {
   return (
     <button
-      className={`bg-dark-green rounded-2xl w-5/6 text-white text-center max-w-xs my-4 min-w-max ${className}`}
+      className={`bg-dark-green rounded-2xl text-white text-center max-w-xs my-4 min-w-max ${className}`}
       {...props}
     >
       {children}
