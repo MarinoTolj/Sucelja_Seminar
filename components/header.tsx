8129b16598701e04ec5cad4802df5bbe721474d0
MarinoTolj <marino.tolj7@gmail.com>
2023-02-15 11:01:13 +0100

    fixed logo position in header

--- components/header.tsx
+++ components/header.tsx
@@ -9,17 +9,19 @@ import Image from "next/image";
 const Header = () => {
   return (
     <header className="bg-main-green-color h-36 flex flex-col justify-between md:flex-row md:items-center md:relative">
-      <div className="flex items-start gap-3 md:order-0">
-        <AccountIcon size={2} />
-        <Link href="/" className="md:order-0">
+      <div className="relative m-auto w-full h-32 md:order-1 md:w-auto">
+        <Link href="/" className="absolute top-9 left-1/3 md:order-0 md:static">
           <Image
             src="/photos/BuyBirdsLogo.png"
             alt="Logo"
-            className="w-24 md:w-64"
+            className="w-32 md:w-64"
             width={200}
             height={100}
           />
         </Link>
+      </div>
+      <div className="flex items-start gap-3 md:order-3">
+        <AccountIcon size={2} />
         <ShoppingCart />
         <HamburgerMenu />
       </div>
